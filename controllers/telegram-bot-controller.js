0346411ae4c061a92e9e95eff62877383b9c3c0e
Adrian Gonzalo <gonzahh@gmail.com>
2017-06-22 01:48:30 +0200

    configured bot to translate to puto

--- controllers/telegram-bot-controller.js
+++ controllers/telegram-bot-controller.js
@@ -1,3 +1,10 @@
+// Requires:
+const adjetiveisor = require('adjetiveisor')
+const lang = require('../lang/default.json')
+
+const puto = adjetiveisor()
+puto.config({ ms: 'puto', mp: 'putos', fs: 'puta', fp: 'putas' })
+
 exports.connectionHandler = () => {
   console.log('Telegram bot is connected')
 }
@@ -11,5 +18,24 @@ exports.textHandler = (msg) => {
 }
 
 exports.commandsHandler = (msg) => {
-
+  const commandRegex = new RegExp('^/\\w*', 'gi')
+  const matches = commandRegex.exec(msg.text)
+  const command = (matches.length) ? matches[0] : ''
+  const text = msg.text.replace(commandRegex, '').trim()
+  switch (command) {
+    case '/puto': putoHandler(text, msg); break
+    case '/ping': pingHandler(msg); break
+    default: commandError(command, msg)
+  }
 }
+
+const commandError = (command, msg) => {
+  return msg.reply.text(`${command} ${lang.IS_NOT_ALLOWED}`, { asReply: false })
+}
+
+const putoHandler = (text, msg) => {
+  const translatedText = puto.translate(text)
+  return msg.reply.text(translatedText, { asReply: true })
+}
+
+const pingHandler = (msg) => msg.reply.text('pong')
